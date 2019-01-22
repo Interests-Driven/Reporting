f16854ee2ed9c518dbc7640a80cf602465777f18
Aron Xu <aron.xu@fa-software.com>
2019-01-22 23:38:18 +0800

    Title  :  service module initial Desc :

---
+++ weekly-service/src/main/java/com/interests/studio/WeeklyApplication.java
@@ -0,0 +1,15 @@
+package com.interests.studio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author aron
+ */
+@SpringBootApplication
+public class WeeklyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeeklyApplication.class, args);
+    }
+}
